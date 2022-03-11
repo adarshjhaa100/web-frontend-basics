fd710b4549d2c99bac32115f311459bfe1b8c38c
adarshjhaa100 <adarshjhaa100@gmail.com>
2022-03-11 18:36:08 +0530

    HTML, CSS basics covered

---
+++ codes/scripts/arraytemp.js
@@ -0,0 +1,9 @@
+// array add elements
+
+const arr = Array.from([1,2,3,4,5]);
+
+console.log("Initial array values:", arr);
+
+// add element
+arr.push(10);
+console.log("Final array values:", arr);

0f1ee5085e9aef644bf327c79c0a3440e7209a1c
adarshjhaa100 <adarshjhaa100@gmail.com>
2022-03-13 21:37:42 +0530

    Add JS notes

--- codes/server/pyhttp101.py
+++ codes/server/pyhttp101.py
@@ -1,3 +1,20 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socket import socket
 
+
+class MyRequestHandler(BaseHTTPRequestHandler):
+    def handle(self) -> None:
+        print("Request method:", self.request)
+        print("Headers:",self.command)
+        return super().handle()
+
+
+
+def run(server_class = HTTPServer, handler_class = MyRequestHandler):
+    server_address = ('',8000) # host and port
+    httpd: HTTPServer = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+run()
 
 

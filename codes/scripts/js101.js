51510bb426457c16fd03b0b8b9179f885e96d146
adarshjhaa100 <adarshjhaa100@gmail.com>
2022-03-20 20:18:56 +0530

    Added array concepts

--- codes/scripts/js101.js
+++ codes/scripts/js101.js
@@ -17,10 +17,17 @@
 
 */
 
+
+/*
+Note: Predicate in JS is a callback functions which returns a boolean
+    e.g. (obj)=> obj.name === "name to find"
+
+*/
+
 /*
     Datatypes and Ops:
-    variables: 
-        - let vs var:
+        variables: 
+            - let vs var:
             var: var is global scope, variables can be used before declaration. THis causes issues.  
             let: let is local scope, variables should be declared before use.
             
@@ -58,6 +65,44 @@
                 substring:          .slice(start, end+1) : String
                 replace:            .replace(originalSubstring, newSubstring) : String
                 uppercase/lowercase:.toUpperCase : String
-                    
+                split a string:     .split(string) : Array
+        Arrays:
+            Arrays are of object type which contains:
+                list of objects (Numbers, Strings etc), which need not be of the same type.
+            Tip: Arrays are optimized by V8 once they are initialized(in case using nodeJS)
+                i.e. if the objects assigned inside an array are all of same type, V* treats it like PACKED_ELEMENTS
+                This allows faster operations.
+                So: Do not access arrays beyond "arr.length". This will slow the optimization the engine may apply.
+            Modification of elements:
+                arr[i] = newValue
+                * Suppose we have an array : [1,2,3,4,5]
+                and we assign: a[10] = 23
+                final array will be: [1,2,3,4,5,undefinedx5,23]
+            Methods:
+                length:                 .length : Number
+                finding items:          .indexOf(element) : Number
+                Adding elements(end):   .push(ele) : Size of new array
+                Adding elements(start): .unshift(ele) : Size of new array
+                Remove(beg):            .shift() : Element  Removed
+                Remove(end):            .pop() : Element Removed
+                Return element:         .find(predicate) : Object
+                Convert to string:      .toString()
+                Convert to string:      .join(join string) 
+            Methods that generate a new array: 
+                Return array after removing R elements from index:
+                    splice(index, R)
+                Return array from start to end:
+                    slice(start, end)
+                Return array with new value which is somehow related to every element:
+                    map((element)=>modifier(element))
+                Filter:
+                    filter(predicate)
+
+            Iteration:
+                for ( ele of arr)
+                    // do something with ele
+        
+
+
 */
 
